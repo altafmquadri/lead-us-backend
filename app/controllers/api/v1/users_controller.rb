ebe7442a6a5afc9c059ade3be18c13322d16283b
Altaf Quadri <altafmquadri@gmail.com>
2019-12-12 14:31:57 -0500

    wrote index method in users to show a list of users with their lead and appointments

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,2 +1,7 @@
-class UsersController < ApplicationController
+class Api::V1::UsersController < ApplicationController
+
+    def index
+        users = User.all
+        render json: users, include: %i[leads appointments], except: %i[created_at updated_at]
+    end
 end
