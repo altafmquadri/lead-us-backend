de930550cc45ced6284b24feb25ec1c179572731
Altaf Quadri <altafmquadri@gmail.com>
2019-12-12 20:58:41 -0500

    added calls and sales to the api

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,10 +1,9 @@
 class Api::V1::UsersController < ApplicationController
 
     def index
-        # leads = Lead.all
         users = User.all
-        # render json: users, include: %i[leads appointments], except: %i[created_at updated_at]
-        render json: { users: users}, include: [:leads, :appointments]
-    
+        render json: { users: users}, include: [:leads, :calls, :appointments, :sales]
     end
 end
+
+# render json: users, include: %i[leads appointments], except: %i[created_at updated_at]
