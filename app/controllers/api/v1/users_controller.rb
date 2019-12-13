5400f8b5678dc83f5f6f91ffe27ff6f6b0e4e760
Altaf Quadri <altafmquadri@gmail.com>
2019-12-12 19:25:02 -0500

    added key of users to backend

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,7 +1,9 @@
 class Api::V1::UsersController < ApplicationController
 
     def index
+        # leads = Lead.all
         users = User.all
-        render json: users, include: %i[leads appointments], except: %i[created_at updated_at]
+        # render json: users, include: %i[leads appointments], except: %i[created_at updated_at]
+        render json: { users: users}, include: [:leads, :appointments]
     end
 end
