e866d608074ad48fc0939b159c8ebd6dc347a9d8
Altaf Quadri <altafmquadri@gmail.com>
2019-12-13 20:03:52 -0500

    Modified the JSON on the backend to handle create request as well as adding a route

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -4,6 +4,17 @@ class Api::V1::UsersController < ApplicationController
         users = User.all
         render json: { users: users}, include: [:leads, :calls, :appointments, :sales]
     end
+
+    def create
+        user = User.find(params[:user_id])
+        user.leads.create(lead_params)
+        render json: user.leads.last
+    end
+
+    private
+    def lead_params
+        params.permit(:user_id, :first_name, :last_name, :street, :city, :state, :postal_code, :phone_number, :beneficiary_information, :date_of_birth)
+    end
 end
 
-# render json: users, include: %i[leads appointments], except: %i[created_at updated_at]
+# render json: users, include: %i[leads appointments], except: %i[created_at updated_a]
