4e29898e2b2e8e916357c92f01c26db9ff61ea38
Altaf Quadri <altafmquadri@gmail.com>
2019-12-14 01:32:17 -0500

    commented a formula for show leads to work on later

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -18,3 +18,6 @@ class Api::V1::UsersController < ApplicationController
 end
 
 # render json: users, include: %i[leads appointments], except: %i[created_at updated_a]
+
+#use this for finding the id for the show page tomorrow 
+# user.leads.find{|lead| lead.id == 1} number will be replaced by the id of the clicked show lead
