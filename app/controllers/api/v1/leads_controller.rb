faf28a1629a4d61b6c2d86db47a2819c4c4aa667
Altaf Quadri <altafmquadri@gmail.com>
2019-12-20 15:32:21 -0500

    wrote a fetch for locationIq api to populate coordinates upon lead creation

--- app/controllers/api/v1/leads_controller.rb
+++ app/controllers/api/v1/leads_controller.rb
@@ -1,7 +1,33 @@
 class Api::V1::LeadsController < ApplicationController
+
+    require 'net/http'
+    require 'json'
+
+    def fetch_vector(params, vector)
+        
+        street = params[:street]
+        city = params[:city]
+        state = params[:state]
+        postalcode = params[:postal_code]
+        format = 'json'
+    
+        url = "https://us1.locationiq.com/v1/search.php?key=#{ENV['locationIQ_api_key']}&street=#{street}&city=#{city}&state=#{state}&postalcode=#{postalcode}&format=#{format}"
+        uri = URI(url)
+
+        response = Net::HTTP.get(uri)
+
+        response = JSON.parse(response)
+        response[0][vector]
+    end
+
     def create
         user = User.find(params[:user_id])
-        user.leads.create(lead_params)
+
+        user.leads.new(lead_params)
+        user.leads.last.update(latitude: fetch_vector(lead_params, 'lat'))
+        user.leads.last.update(longitude: fetch_vector(lead_params, 'lon'))
+        user.leads.last.save
+
         render json: user.leads.last
     end
 
