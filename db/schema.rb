b3addabcb27002d4231fc6938e9e9c63c69ded81
Altaf Quadri <altafmquadri@gmail.com>
2020-01-04 17:17:14 -0500

    changed procfile to run on heroku

--- db/schema.rb
+++ db/schema.rb
@@ -12,15 +12,18 @@
 
 ActiveRecord::Schema.define(version: 2019_12_17_194625) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "appointments", force: :cascade do |t|
-    t.integer "lead_id", null: false
+    t.bigint "lead_id", null: false
     t.string "title"
     t.date "date"
     t.datetime "start_time"
     t.datetime "end_time"
     t.boolean "presentation_made?", default: false
     t.boolean "made_sale?", default: false
-    t.integer "user_id", null: false
+    t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["lead_id"], name: "index_appointments_on_lead_id"
@@ -28,11 +31,11 @@ ActiveRecord::Schema.define(version: 2019_12_17_194625) do
   end
 
   create_table "calls", force: :cascade do |t|
-    t.integer "lead_id", null: false
+    t.bigint "lead_id", null: false
     t.string "call_status"
     t.boolean "appointment_made?", default: false
     t.boolean "archive_lead?", default: false
-    t.integer "user_id", null: false
+    t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["lead_id"], name: "index_calls_on_lead_id"
@@ -55,16 +58,16 @@ ActiveRecord::Schema.define(version: 2019_12_17_194625) do
     t.boolean "lead_archived?", default: false
     t.decimal "latitude", precision: 10, scale: 6, default: "0.0"
     t.decimal "longitude", precision: 10, scale: 6, default: "0.0"
-    t.integer "user_id", null: false
+    t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["user_id"], name: "index_leads_on_user_id"
   end
 
   create_table "sales", force: :cascade do |t|
-    t.integer "lead_id", null: false
+    t.bigint "lead_id", null: false
     t.float "annualized_life_premium", default: 0.0
-    t.integer "user_id", null: false
+    t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["lead_id"], name: "index_sales_on_lead_id"
